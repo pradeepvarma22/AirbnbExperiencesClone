2f03fb690661f47624baf9be91d498d9d0d1cbde
pradeepvarma22 <pradeepnadimpalli22@gmail.com>
2022-03-12 08:30:09 +0530

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Imagegrid from '../images/image-grid.png'
-import './Hero.css'
+import './Hero.css';
 
 
 
 export default function Hero() {
     return (
         <section>
-            <p className='p-img'><img className='image-grid' src={Imagegrid} /></p>
+            <p className='p-img'><img className='image-grid' src='./images/image-grid.png' /></p>
             <h1 className='hero-header'>Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by
                 one-of-a-kind hosts—all without leaving home.</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import airBnbLogo from '../images/airbnb_logo.png';
 import './Navbar.css';
 
 
@@ -10,7 +9,7 @@ export default function Navbar() {
     return (
         <div className="navbar-div">
             <navbar>
-                <img className="image-logo" src={airBnbLogo} />
+                <img className="image-logo" src='./images/airbnb_logo.png' />
             </navbar>
         </div >
     );
